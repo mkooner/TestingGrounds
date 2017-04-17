88ec74613566178e3c9e8bbbb79d5ffe197b227c
Mehrab Kooner <mkooner@live.ca>
2017-04-17 00:51:32 -0400

    AI Patrol and Chase Behaviors added

--- Source/TestingGrounds/Private/MyCharacter.cpp
+++ Source/TestingGrounds/Private/MyCharacter.cpp
@@ -7,23 +7,55 @@
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
+	/* Create a CameraComponent	
+	ThirdPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	ThirdPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	ThirdPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
+	ThirdPersonCameraComponent->bUsePawnControlRotation = true;
+	*/
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh3P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	//Mesh3P->SetOnlyOwnerSee(true);
+	Mesh3P->SetupAttachment(GetCapsuleComponent());
+	Mesh3P->bCastDynamicShadow = false;
+	Mesh3P->CastShadow = false;
+	Mesh3P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	Mesh3P->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	// Create a gun mesh component
+	FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+	//FP_Gun->SetOnlyOwnerSee(true);			// only the owning player will see this mesh
+	FP_Gun->bCastDynamicShadow = false;
+	FP_Gun->CastShadow = false;
+	FP_Gun->SetupAttachment(Mesh3P, TEXT("GripPoint"));
+	//FP_Gun->SetupAttachment(RootComponent);
 
-	//create dynamic material anywhere u like, Constructor or anywhere .
-	UMaterialInstanceDynamic* DynMaterial = UMaterialInstanceDynamic::Create(Material, this, FName("Material"));
-	//set paramater with Set***ParamaterValue
-	//DynMaterial->SetScalarParameterValue("MyParameter", myFloatValue);
-	//MyComponent1->SetMaterial(0, DynMaterial);
-	//MyComponent2->SetMaterial(0, DynMaterial);
-	//...
+	FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
+	FP_MuzzleLocation->SetupAttachment(FP_Gun);
+	FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
+
+	// Default offset from the character location for projectiles to spawn
+	GunOffset = FVector(100.0f, 0.0f, 10.0f);
+	
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
+	//FP_Gun->AttachToComponent(Mesh3P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	//Mesh3P->SetHiddenInGame(false, true);
 }
 
 // Called every frame
--- Source/TestingGrounds/Public/MyCharacter.h
+++ Source/TestingGrounds/Public/MyCharacter.h
@@ -10,6 +10,22 @@ class TESTINGGROUNDS_API AMyCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+		UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* Mesh3P;
+
+	/** Gun mesh: 1st person view (seen only by self) */
+		UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* FP_Gun;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USceneComponent* FP_MuzzleLocation;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* ThirdPersonCameraComponent;
+
 public:
 	// Sets default values for this character's properties
 	AMyCharacter();
@@ -17,6 +33,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
 
 public:	
 	// Called every frame
@@ -25,8 +42,15 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(EditAnywhere)
-		UMaterialInterface* Material;
+	/** Gun muzzle's offset from the characters location */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FVector GunOffset;
 
-	
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseLookUpRate;
 };
